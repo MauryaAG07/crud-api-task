0a2775434f4dfe8d77e3fcaffbca338a72635ee9
Maurya Govindu <maurya@Mauryas-MacBook-Pro.local>
2026-08-19 01:14:29 -0500

    Stage 5: Swagger UI

--- main.py
+++ main.py
@@ -18,21 +18,25 @@ class TaskUpdate(BaseModel):
 
 @app.get("/")
 def root():
+    """Returns basic information qnd metadata about the API"""
     return {"name": "Task API", "version": "1.0", "endpoints": ["/tasks"]}
 
 
 @app.get("/health")
 def health():
+    """Diagnostic endpoint to check if the server is running successfully"""
     return {"status": "ok"}
 
 
 @app.get("/tasks")
 def read_tasks():
+    """Retrieves a list of all current tasks"""
     return tasks
 
 
 @app.get("/tasks/{task_id}")
 def read_task(task_id: int, response: Response):
+    """Retrieves a single specific task by its ID number."""
     for task in tasks:
         if task["id"] == task_id:
             return task
@@ -42,6 +46,7 @@ def read_task(task_id: int, response: Response):
 
 @app.post("/tasks")
 def create_task(task_input: TaskInput, response: Response):
+    """Creates a new task, requiring a valid title in the request"""
     if not task_input.title or not task_input.title.strip():
         response.status_code = 400
         return {"error": "Title is missing or empty"}
@@ -54,6 +59,7 @@ def create_task(task_input: TaskInput, response: Response):
     return new_task
 @app.put("/tasks/{task_id}")
 def update_task(task_id: int, task_update: TaskUpdate, response: Response):
+    """Updates an existing task's title and/or completion status."""
     if task_update.title is None and task_update.done is None:
         response.status_code = 400
         return {"error": "Request is missing or empty"}
@@ -73,6 +79,7 @@ def update_task(task_id: int, task_update: TaskUpdate, response: Response):
 
 @app.delete("/tasks/{task_id}")
 def delete_task(task_id: int, response: Response):
+    """Permanently deletes an existing task, found by its ID number"""
     for i, task in enumerate(tasks):
         if task["id"] == task_id:
             tasks.pop(i)
